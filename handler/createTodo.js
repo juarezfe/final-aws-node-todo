667f1f709f6336196e524a7df6548588164bc759
Jose Juarez <jose.juarez@levadata.com>
2023-03-02 14:41:47 -0600

    using dynamoDB PutCommand to send response correctly

--- handler/createTodo.js
+++ handler/createTodo.js
@@ -1,11 +1,15 @@
-const AWS = require("aws-sdk");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  PutCommand
+} = require("@aws-sdk/lib-dynamodb");
 const uuid = require("uuid")
-
 const TODO_TABLE = process.env.TODO_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const client = new DynamoDBClient();
+const dynamoDb = DynamoDBDocumentClient.from(client);
 
 module.exports.createTodo = async (event, context, callback) => {
-    const timestamp =  new Date().getTime()
+    const timestamp = new Date().getTime()
     const data = JSON.parse(event.body)
     if (typeof data.todo !== "string") {
         console.error("Validation Failed");
@@ -15,7 +19,7 @@ module.exports.createTodo = async (event, context, callback) => {
     const params = {
         TableName: TODO_TABLE,
         Item: {
-            userId: uuid.v1(),
+            id: uuid.v1(),
             todo: data.todo,
             checked: false,
             createdAt: timestamp,
@@ -23,18 +27,15 @@ module.exports.createTodo = async (event, context, callback) => {
         }
     }
 
-    dynamoDb.put(params,(error, data) => {
-        if (error) {
-            console.error(error);
-            callback(new Error(error));
-            return;
-        }
-        // create a response
-        const response =  {
+    // Send the PutCommand to DynamoDB
+    try {
+        const data = await dynamoDb.send(new PutCommand(params));
+        const response = {
             statusCode: 200,
             body: JSON.stringify(data.Item)
-        }
+        };
         callback(null, response)
-    })
-
+    } catch (error) {
+        callback(new Error(error));
+    }
 }
